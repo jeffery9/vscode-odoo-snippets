8802da932635d682ebf6ba24faf8b7aaf2f21da3
jeffery chen fan <jeffery9@outlook.com>
2019-07-03 13:22:13 +0800

    sync with sublime-odoo-snippets

--- source/convert.py
+++ source/convert.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python2
 import simplejson as json
 
 with open('data.json', 'r') as f:
@@ -14,17 +15,17 @@ for k in  data.iterkeys():
     description = data[k]['description']
     bl = body.split('\n')
     data[k]['body'] = bl
-
+   
     new_data ={}
     new_data.update( { 'body': bl, 'prefix':prefix, 'description': description })
 
-    if scope == 'source.python':
+    if 'source.python' in scope:
         python.update( { k: new_data })
 
-    if scope == 'text.xml' or scope == 'text.xml meta.tag.xml':
+    if 'text.xml' in scope:
         xml.update( { k: new_data })
 
-with open('snippets/python.json', 'w') as p:
+with open('../snippets/python.json', 'w') as p:
     p.write(json.dumps(python, indent=4, sort_keys=True))
-with open('snippets/xml.json', 'w') as x:
+with open('../snippets/xml.json', 'w') as x:
     x.write(json.dumps(xml, indent=4, sort_keys=True))
